873931269469bc8955c40bcd11177776c46ecb18
PrintHolamundo <jdohca@gmail.com>
2022-03-27 11:26:43 -0600

    Estructura del proyecto y Componentes

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Quien es este pokemon?</h1>
+  <PokemonPicture />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+
+export default {
+  components: { PokemonOptions, PokemonPicture },
+};
+</script>
+
+<style></style>

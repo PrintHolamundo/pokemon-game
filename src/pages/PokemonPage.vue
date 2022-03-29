7f1a801c7dfa51eee8b503bb8b60445aaccbb8f5
PrintHolamundo <jdohca@gmail.com>
2022-03-29 14:03:06 -0600

    Logica de los nombres de los pokemon

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -7,6 +7,9 @@
 <script>
 import PokemonOptions from "@/components/PokemonOptions.vue";
 import PokemonPicture from "@/components/PokemonPicture.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+console.log(getPokemonOptions());
 
 export default {
   components: { PokemonOptions, PokemonPicture },

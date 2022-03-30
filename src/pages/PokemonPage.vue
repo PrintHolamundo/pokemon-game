b198aa0ac1ff2a06bb92bae52d1463c61ee37e0c
PrintHolamundo <jdohca@gmail.com>
2022-03-30 12:34:43 -0600

    Obtener nombres de los 4 pokemons

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,7 +9,6 @@ import PokemonOptions from "@/components/PokemonOptions.vue";
 import PokemonPicture from "@/components/PokemonPicture.vue";
 import getPokemonOptions from "@/helpers/getPokemonOptions";
 
-console.log(getPokemonOptions());
 
 export default {
   components: { PokemonOptions, PokemonPicture },

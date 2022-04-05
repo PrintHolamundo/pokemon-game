6fa7f9896e6fdb1ef2bc225f558a8bd54552fb7e
PrintHolamundo <jdohca@gmail.com>
2022-04-05 15:44:23 -0500

    Declaracion de Pruebas

--- tests/unit/helpers/getPokemonOptions.spec.js
+++ tests/unit/helpers/getPokemonOptions.spec.js
@@ -1,14 +1,15 @@
-import {getPokemons} from '@/helpers/getPokemonOptions'
+import { getPokemons } from "@/helpers/getPokemonOptions";
 
-describe('getPokemonOptions helpers', () => { 
-    test('debe de regresar un arreglo de numeros',() =>{
-        const pokemons = getPokemons()
-        expect(pokemons.length).toBe(650)
-        expect(pokemons[649]).toBe(650)
-        expect(pokemons[500]).toBe(501)
-        expect(pokemons[649]).toBe(650)
+describe("getPokemonOptions helpers", () => {
+    test("debe de regresar un arreglo de numeros", () => {
+        const pokemons = getPokemons();
+        expect(pokemons.length).toBe(650);
+        expect(pokemons[649]).toBe(650);
+        expect(pokemons[500]).toBe(501);
+        expect(pokemons[649]).toBe(650);
+    });
 
-    
-    })
+    test("debe de retornar un arreglo de 4 elementos con nombres de pokemons", () => { });
 
-})
+    test("getPokemonsOptions debe de retornar un arreglo mezclado", () => { });
+});

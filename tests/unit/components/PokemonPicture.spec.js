6472306986e23e7c49fc518d7abe9d1846c4338b
PrintHolamundo <jdohca@gmail.com>
2022-04-06 11:12:00 -0500

    Pruebas de atributos

--- tests/unit/components/PokemonPicture.spec.js
+++ tests/unit/components/PokemonPicture.spec.js
@@ -17,10 +17,30 @@ describe('PokemonPicture', () => {
     })
 
     test('debe de mostrar la imagen oculta y el pokemon 100', () => {
-
+        const wrapper = shallowMount(PokemonPicture, {
+            props: {
+                pokemonId: 100,
+                showpokemon: false,
+            }
+        })
+        const [img1, img2] = wrapper.findAll('img')
+        expect(img1.exists()).toBeTruthy()
+        expect(img2).toBe(undefined)
+        expect(img1.classes('hidden-pokemon')).toBeTruthy()
+        expect(img1.attributes('src')).toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg')
     })
 
     test('debe de mostrar el pokemon si showpokemon:true', () => {
+        const wrapper = shallowMount(PokemonPicture, {
+            props: {
+                pokemonId: 100,
+                showpokemon: true,
+            }
+        })
+        const img1 = wrapper.find('img')
+        expect(img1.exists()).toBeTruthy()
+        expect(img1.classes('hidden-pokemon')).toBe(true)
+        expect(img1.classes('fade-in')).toBe(false)
 
     })
 })

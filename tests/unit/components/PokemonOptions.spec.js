02f43535a357d12c67928f528ddb14353b1c3f20
PrintHolamundo <jdohca@gmail.com>
2022-04-06 20:21:54 -0500

    Pruebas con emisiones

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -12,8 +12,20 @@ describe('PokemonOptions Component', () => {
         })
     })
     test('debe de hacer match con el snapshot', () => {
-        console.log( wrapper.html())
         expect(wrapper.html()).toMatchSnapshot()
 
+    })
+    test('debe de mostrar las 4 opciones li correctamente', () => {
+        expect(wrapper.findAll('li').length).toBe(4)
+        expect(wrapper.findAll('li').at(0).text()).toBe('bulbasaur')
+        expect(wrapper.findAll('li').at(1).text()).toBe('ivysaur')
+        expect(wrapper.findAll('li').at(2).text()).toBe('venusaur')
+        expect(wrapper.findAll('li').at(3).text()).toBe('charmander')
+
+    })
+    test('debe de emitir "selection" con sus respectivos parametros al hacer click' , () => {
+        wrapper.findAll('li').at(0).trigger('click')
+        expect(wrapper.emitted().selection).toBeTruthy()
+        expect(wrapper.emitted().selection[0]).toEqual([1])
     })
  })

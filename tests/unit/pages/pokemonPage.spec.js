07d72e9317e1dc43a7d7d8db9fcf9c51c584ea38
PrintHolamundo <jdohca@gmail.com>
2022-04-08 11:37:00 -0500

    Snapshot con data y stubs

--- tests/unit/pages/pokemonPage.spec.js
+++ tests/unit/pages/pokemonPage.spec.js
@@ -1,25 +1,40 @@
-import { shallowMount, mount } from '@vue/test-utils'
-import PokemonPage from '@/pages/PokemonPage'
-import { pokemons } from '../mocks/pokemons.mock'
+import { shallowMount, mount } from "@vue/test-utils";
+import PokemonPage from "@/pages/PokemonPage";
+import { pokemons } from "../mocks/pokemons.mock";
 
-describe('PokemonPage Component', () => {
-    
-    let wrapper
+describe("PokemonPage Component", () => {
+    let wrapper;
 
     beforeEach(() => {
-        wrapper = shallowMount( PokemonPage )
-    })
+        wrapper = shallowMount(PokemonPage);
+    });
 
-    test('debe de hacer match con el snapshot', () => {        
-        expect( wrapper.html() ).toMatchSnapshot()
-    })
+    test("debe de hacer match con el snapshot", () => {
+        expect(wrapper.html()).toMatchSnapshot();
+    });
 
-    test('debe de llamar mixPokemonArray al montar', () => {
-        
-        const mixPokemonArraySpy = jest.spyOn( PokemonPage.methods, 'mixPokemonArray' )
-        const wrapper = shallowMount( PokemonPage )
-        
-        expect( mixPokemonArraySpy ).toHaveBeenCalled()
-    })
+    test("debe de llamar mixPokemonArray al montar", () => {
+        const mixPokemonArraySpy = jest.spyOn(
+            PokemonPage.methods,
+            "mixPokemonArray"
+        );
+        const wrapper = shallowMount(PokemonPage);
 
+        expect(mixPokemonArraySpy).toHaveBeenCalled();
+    });
+
+    test("debe de hacer match con el snapshot cuando cargan  los pokemons", () => {
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ""
+                }
+            },
+        })
+        expect(wrapper.html()).toMatchSnapshot();
+    })
 })
